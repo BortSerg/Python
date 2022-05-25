4de760874aeceddb78448a1c4a1780d2ccd4b9e1
Bort Serg <bort.sergei@gmail.com>
2022-05-25 17:38:22 +0300

    classes for hub, serial port data and logging

---
+++ my_logging.py
@@ -0,0 +1,61 @@
+from sys import platform
+from os import getlogin, getcwd
+from datetime import datetime
+from pathlib import Path
+from os import chown, chmod
+
+
+class Logging(object):
+
+    def __init__(self):
+        self.__path = None
+        self.__name_log_file = None
+        self.os_system = platform
+
+    def get_name_log_file(self):
+        return self.__name_log_file
+
+    def get_path_log_file(self):
+        self.__get_path()
+        return self.__path
+
+    def __get_path(self):
+        path = None
+        file_name = None
+
+        if self.os_system in {'linux', 'linux2'}:
+            path = f"/home/{getlogin()}/Log/"
+            file_name = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.txt"
+
+        if self.os_system == 'win32':
+            path_line = getcwd().split("\\")
+            path = path_line[0] + "\\Log\\"
+            f"{datetime.now().strftime('%Y-%m-%d %H-%M-%S')}.txt"
+
+        Path(path).mkdir(0o777, parents=True, exist_ok=True)
+        self.__path = path
+        self.__name_log_file = file_name
+
+    def __create_auto_log_file(self):
+        file = open(self.__path + self.__name_log_file, 'w')
+        file.close()
+
+    def crete_log_file(self, name_log_file=None):
+        self.__get_path()
+        if name_log_file is None:
+            self.__create_auto_log_file()
+
+        else:
+            self.__name_log_file = name_log_file
+            file = open(self.__path + self.__name_log_file, 'w')
+            file.close()
+
+        chmod(file.name, 0o777)
+        chown(file.name, 1000, 1000)
+
+    def write_log(self, message: str):
+        if self.get_name_log_file() is None:
+            self.__get_path()
+            self.__create_auto_log_file()
+        with open(self.__path + self.__name_log_file, 'a') as log:
+            log.write(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} ] {message}")
---
+++ my_serialdata.py
@@ -0,0 +1,55 @@
+import serial
+import re
+from my_logging import Logging
+
+
+class SerialData(Logging):
+
+    def __init__(self):
+        Logging.__init__(self)
+        self.__port = None
+        self.__speed = None
+        self.__serial_port = None
+        self.__autolog = True
+        self.__print_data = False
+
+    def set_autolog(self, flag: bool):
+        self.__autolog = flag
+
+    def set_print_data(self, flag: bool):
+        self.__print_data = flag
+
+    def get_port(self):
+        return self.__port
+
+    def set_serial_port(self, port, speed: int):
+        self.__serial_port = serial.Serial("/dev/ttyUSB" + str(port), speed)
+        self.__port = port
+        self.__speed = speed
+
+    def read_serial(self):
+        def anti_esc(line):
+            ansi_escape = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]')
+            line = ansi_escape.sub('', line).replace('>\r', '>')
+            return line
+
+        line = self.__serial_port.readline().decode('utf-8')
+        line = anti_esc(line)
+        if self.__autolog:
+            self.write_log(line)
+        if self.__print_data:
+            print(line)
+        return line
+
+    def write_serial(self, message: str):
+        self.__serial_port.write((message + "\r\n").encode('UTF-8'))
+
+
+class Hub(SerialData):
+    def __init__(self):
+        SerialData.__init__(self)
+        self.hub_id = None
+        self.hub_name = None
+        self.relay_norm_id = None
+        self.relay_low_id = None
+        self.socket_id = None
